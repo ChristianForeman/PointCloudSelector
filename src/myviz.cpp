b8c13056debaa39f0c57679c018d016abd591a72
ChristianForeman <cjforema@umich.edu>
2022-05-25 14:18:42 -0400

    added a panel that is separate from rviz, now trying to have it within rviz

---
+++ src/myviz.cpp
@@ -0,0 +1,81 @@
+// Reference: http://docs.ros.org/en/lunar/api/librviz_tutorial/html/index.html
+#include <QColor>
+#include <QSlider>
+#include <QLabel>
+#include <QGridLayout>
+#include <QVBoxLayout>
+
+#include "rviz/visualization_manager.h"
+#include "rviz/render_panel.h"
+#include "rviz/display.h"
+
+#include "myviz.h"
+
+// BEGIN_TUTORIAL
+// Constructor for MyViz.  This does most of the work of the class.
+MyViz::MyViz( QWidget* parent )
+  : QWidget( parent )
+{
+  // Construct and lay out labels and slider controls.
+  QLabel* thickness_label = new QLabel( "Line Thickness" );
+  QSlider* thickness_slider = new QSlider( Qt::Horizontal );
+  thickness_slider->setMinimum( 1 );
+  thickness_slider->setMaximum( 100 );
+  QLabel* cell_size_label = new QLabel( "Cell Size" );
+  QSlider* cell_size_slider = new QSlider( Qt::Horizontal );
+  cell_size_slider->setMinimum( 1 );
+  cell_size_slider->setMaximum( 100 );
+  QGridLayout* controls_layout = new QGridLayout();
+  controls_layout->addWidget( thickness_label, 0, 0 );
+  controls_layout->addWidget( thickness_slider, 0, 1 );
+  controls_layout->addWidget( cell_size_label, 1, 0 );
+  controls_layout->addWidget( cell_size_slider, 1, 1 );
+
+  // Construct and lay out render panel.
+  render_panel_ = new rviz::RenderPanel();
+  QVBoxLayout* main_layout = new QVBoxLayout;
+  main_layout->addLayout( controls_layout );
+
+  // Set the top-level layout for this MyViz widget.
+  setLayout( main_layout );
+
+  // Make signal/slot connections.
+  connect( thickness_slider, SIGNAL( valueChanged( int )), this, SLOT( setThickness( int )));
+  connect( cell_size_slider, SIGNAL( valueChanged( int )), this, SLOT( setCellSize( int )));
+
+  // Next we initialize the main RViz classes.
+  //
+  // The VisualizationManager is the container for Display objects,
+  // holds the main Ogre scene, holds the ViewController, etc.  It is
+  // very central and we will probably need one in every usage of
+  // librviz.
+  manager_ = new rviz::VisualizationManager( render_panel_ );
+  manager_->initialize();
+  manager_->startUpdate();
+
+  // Initialize the slider values.
+  thickness_slider->setValue( 25 );
+  cell_size_slider->setValue( 10 );
+}
+
+// Destructor.
+MyViz::~MyViz()
+{
+  delete manager_;
+}
+
+// This function is a Qt slot connected to a QSlider's valueChanged()
+// signal.  It sets the line thickness of the grid by changing the
+// grid's "Line Width" property.
+void MyViz::setThickness( int thickness_percent )
+{
+  return;
+}
+
+// This function is a Qt slot connected to a QSlider's valueChanged()
+// signal.  It sets the cell size of the grid by changing the grid's
+// "Cell Size" Property.
+void MyViz::setCellSize( int cell_size_percent )
+{
+  return;
+}
