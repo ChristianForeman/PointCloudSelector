76b0a6b8f3f09b42c40031b700035cd274d6aa4b
ChristianForeman <cjforema@umich.edu>
2022-06-01 16:05:01 -0400

    xyz to rosbag now normalizes the pointcloud

--- src/ply_to_bag.py
+++ src/ply_to_bag.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import rospy
 from sensor_msgs import point_cloud2
-
+from sensor_msgs.msg import PointCloud2
 from sensor_msgs.msg import PointField
 from std_msgs.msg import Header
 
@@ -30,12 +30,26 @@ def points_to_pc2_msg(points, frame_id):
 def xyz_to_pc2msg(filepath):
     A = np.loadtxt(filepath)
     A = A[~np.all(A == 0, axis=1)]
+
+    # Do some normalization so it isn't massive in rviz
+    mean = np.sum(A, axis=0) / A.shape[0]
+    A = A - mean
+    A = A / 100
+
     pc2_msg = points_to_pc2_msg(A, "camera_depth_optical_frame")
     return pc2_msg
 
 
-def main():
+def publish_time():
     rospy.init_node("xyz_to_pointcloud")
+    pc2_msg = xyz_to_pc2msg("/home/christianforeman/catkin_ws/src/point_cloud_selector/pcs/full_scan_3_binary.xyz")
+    pub = rospy.Publisher("/sel_data/cur_frame", PointCloud2, queue_size=10)
+    while True:
+        rospy.sleep(5)
+        pub.publish(pc2_msg)
+
+
+def main():
     pc2_msg1 = xyz_to_pc2msg("/home/christianforeman/catkin_ws/src/point_cloud_selector/pcs/scan_1_cropped.xyz")
     pc2_msg2 = xyz_to_pc2msg("/home/christianforeman/catkin_ws/src/point_cloud_selector/pcs/scan_2_cropped.xyz")
     pc2_msg3 = xyz_to_pc2msg("/home/christianforeman/catkin_ws/src/point_cloud_selector/pcs/scan_3_cropped.xyz")
@@ -46,7 +60,7 @@ def main():
     bag.write('/camera/depth/color/points', pc2_msg3)
     bag.close()
 
-    # publish_time(pub, pc2_msg)
+    # publish_time()
 
     
 if __name__ == "__main__":
